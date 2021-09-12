b1d3415df43988f7d95d110293d94b3c67d3e76a
whitgroves <whitney.groves@gmail.com>
2021-09-11 20:57:15 -0500

    authcard render tests

--- src/components/AuthCard.js
+++ src/components/AuthCard.js
@@ -19,13 +19,11 @@ export default function AuthCard(props) {
   const [error, setError] = useState('');
 
   useEffect(() => {
-    // console.log(`error is: ${error}`);
     setShowError(error.length !== 0);
-    // setValidated(error.length === 0);
   }, [error]);
 
   const handleSubmit = (event) => {
-    
+
     event.preventDefault();
 
     let password = event.target.password.value;
@@ -40,29 +38,29 @@ export default function AuthCard(props) {
       }
     }
 
-    if (!isRegistration || !hasRegistrationError){
+    if (!isRegistration || !hasRegistrationError) {
       const form = event.currentTarget;
 
       if (form.checkValidity() === false) {
         event.stopPropagation();
-      } 
+      }
       else {
         let email = event.target.email.value;
         let endpoint = isRegistration ? '/auth/register' : '/auth/login';
 
         fetch(endpoint, {
           method: 'POST',
-          headers: {'Content-Type': 'application/json'},
+          headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
             email: email,
             password: password,
           })
 
         }).then(res => res.json()).then(data => {
-          
+
           if (data['status'] === 500) {
             setError(data['error']);
-          } 
+          }
           else {
             setError('');
             props.onAuthenticated(data);
@@ -79,10 +77,10 @@ export default function AuthCard(props) {
   return (
     <Card>
       <Card.Body>
-        <Card.Title>{isRegistration ? 'sign up' : 'sign in'}</Card.Title>
+        <Card.Title data-testid="auth-card-title">{isRegistration ? 'sign up' : 'sign in'}</Card.Title>
         <Form noValidate validated={validated} onSubmit={handleSubmit}>
 
-          <Form.Group controlId="email">
+          <Form.Group controlId="email" data-testid="auth-card-email">
             <Form.Label>email</Form.Label>
             <Form.Control type="email" required />
 
@@ -95,9 +93,9 @@ export default function AuthCard(props) {
             </Form.Text>
           </Form.Group>
 
-          <div className="py-2"/>
+          <div className="py-2" />
 
-          <Form.Group controlId="password">
+          <Form.Group controlId="password" data-testid="auth-card-password">
             <Form.Label>password</Form.Label>
             <Form.Control type="password" required />
 
@@ -108,10 +106,10 @@ export default function AuthCard(props) {
             <Form.Text className="text-muted">
               We can't see this.
               {' '}
-              <a 
-                href="https://security.stackexchange.com/q/33860" 
-                target="_blank" 
-                rel="noreferrer" 
+              <a
+                href="https://security.stackexchange.com/q/33860"
+                target="_blank"
+                rel="noreferrer"
                 tabIndex={-1}
               >
                 Learn more.
@@ -119,32 +117,32 @@ export default function AuthCard(props) {
             </Form.Text>
           </Form.Group>
 
-          <div className="py-2"/>
+          <div className="py-2" />
 
-          { 
-            isRegistration 
-              ? <Form.Group controlId="password_confirm">
-                  <Form.Label>confirm password</Form.Label>
-                  <Form.Control type="password" required />
+          {
+            isRegistration
+              ? <Form.Group controlId="password_confirm" data-testid="auth-card-password-confirm">
+                <Form.Label>confirm password</Form.Label>
+                <Form.Control type="password" required />
 
-                  <Form.Control.Feedback type="invalid">
-                    Passwords re-enter your password.
-                  </Form.Control.Feedback>
-                </Form.Group>
+                <Form.Control.Feedback type="invalid">
+                  Passwords re-enter your password.
+                </Form.Control.Feedback>
+              </Form.Group>
               : ''
           }
 
-          { 
-            isRegistration 
+          {
+            isRegistration
               ? <div className="py-2" />  // don't want to group with password_confirm
               : ''
           }
 
           <Row>
             <Col className="d-inline-flex align-items-center" md="auto">
-              { 
-                showError 
-                  ? <Badge bg="danger">{error}</Badge> 
+              {
+                showError
+                  ? <Badge bg="danger">{error}</Badge>
                   : ''
               }
             </Col>
@@ -152,7 +150,9 @@ export default function AuthCard(props) {
             <Col></Col> {/* spacer */}
 
             <Col md="auto">
-              <Button variant="primary" type="submit">{isRegistration ? 'sign up' : 'sign in'}</Button>
+              <Button data-testid="auth-submit-button" variant="primary" type="submit">
+                {isRegistration ? 'sign up' : 'sign in'}
+              </Button>
             </Col>
           </Row>
 
---
+++ src/components/AuthCard.test.js
@@ -0,0 +1,51 @@
+import { render, screen } from '@testing-library/react'
+import AuthCard from './AuthCard'
+
+test('renders as login', () => {
+  render(
+    <AuthCard
+      isRegistration={false}
+      onAuthenticated={() => { }}
+    />
+  );
+
+  const titleElement = screen.getByTestId('auth-card-title');
+  expect(titleElement).toBeInTheDocument();
+  expect(titleElement.innerText).toEqual < String > ('sign in');
+
+  const emailField = screen.getByTestId('auth-card-email');
+  expect(emailField).toBeInTheDocument();
+
+  const passwordField = screen.getByTestId('auth-card-password');
+  expect(passwordField).toBeInTheDocument();
+
+  const submitButton = screen.getByTestId('auth-submit-button');
+  expect(submitButton).toBeInTheDocument();
+  expect(submitButton.innerText).toEqual < String > ('sign in');
+})
+
+test('renders as registration', () => {
+  render(
+    <AuthCard
+      isRegistration={true}
+      onAuthenticated={() => { }}
+    />
+  );
+
+  const titleElement = screen.getByTestId('auth-card-title');
+  expect(titleElement).toBeInTheDocument();
+  expect(titleElement.innerText).toEqual < String > ('sign up');
+
+  const emailField = screen.getByTestId('auth-card-email');
+  expect(emailField).toBeInTheDocument();
+
+  const passwordField = screen.getByTestId('auth-card-password');
+  expect(passwordField).toBeInTheDocument();
+
+  const passwordConfirmField = screen.getByTestId('auth-card-password-confirm');
+  expect(passwordConfirmField).toBeInTheDocument();
+
+  const submitButton = screen.getByTestId('auth-submit-button');
+  expect(submitButton).toBeInTheDocument();
+  expect(submitButton.innerText).toEqual < String > ('sign up');
+})
